1659399a6ed8b3589c4cf15b6608b4127df01846
Hồng Đức <duc.tran5@ntq-solution.com.vn>
2026-07-13 17:22:52 +0700

    test cicd

--- src/main/java/com/iwas/IWASApplication.java
+++ src/main/java/com/iwas/IWASApplication.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @EnableAsync
+
 public class IWASApplication {
 
 	public static void main(String[] args) {
